fac5f9f6b83502089496a1cba25fc586e82ba6b5
snehith264 <95459136+snehith264@users.noreply.github.com>
2023-06-06 10:38:13 +0530

    Add files via upload

---
+++ nextpermutation.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+    void nextPermutation(vector<int> &nums)
+    {
+        int ind = -1;
+        for (int i = nums.size() - 2; i >= 0; i--)
+        {
+            if (nums[i] < nums[i + 1])
+            {
+                ind = i;
+                break;
+            }
+        }
+        if (ind == -1)
+        {
+            reverse(nums.begin(), nums.end());
+        }
+        else
+        {
+            for (int i = nums.size() - 1; i >= 0; i--)
+            {
+                if (nums[i] > nums[ind])
+                {
+                    swap(nums[i], nums[ind]);
+                    break;
+                }
+            }
+            for (int i = 0; i < nums.size(); ++i)
+            {
+                cout << nums[i] << '\t';
+            }
+            cout << '\n';
+            sort(nums.begin() + ind + 1, nums.end());
+        }
+    }
+};
